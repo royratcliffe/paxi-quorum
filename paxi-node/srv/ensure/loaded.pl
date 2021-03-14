5b95169a73ad41e6b8f6b41e72d5f42cc3f7ad01
Roy Ratcliffe <royratcliffe@me.com>
2021-03-14 18:59:43 +0000

    Ensure loaded on program goal

--- paxi-node/srv/ensure/loaded.pl
+++ paxi-node/srv/ensure/loaded.pl
@@ -1,4 +1,8 @@
-:-
+:- module(ensure_loaded, []).
+
+:- initialization(up, program).
+
+up :-
     expand_file_name('[0-9]*.pl', Files0),
     sort(Files0, Files),
     load_files(Files, [if(not_loaded)]).
