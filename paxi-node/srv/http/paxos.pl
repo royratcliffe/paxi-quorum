b0d6c0af172a33cc275a5504289fb54e9e88971b
Roy Ratcliffe <royratcliffe@me.com>
2021-03-15 18:54:40 +0000

    Paxos ledger handler

--- paxi-node/srv/http/paxos.pl
+++ paxi-node/srv/http/paxos.pl
@@ -2,10 +2,12 @@
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
 :- use_module(library(paxos)).
+:- use_module(paxos/ledger).
 
 :- http_handler(root(paxos), paxos, []).
 :- http_handler(root(paxos/node), paxos_node, []).
 :- http_handler(root(paxos/quorum), paxos_quorum, []).
+:- http_handler(root(paxos/ledger), paxos_ledger, []).
 
 paxos(_Request) :-
     findall(B=C, (paxos_property(A), A =.. [B, C]), D),
@@ -18,3 +20,7 @@ paxos_node(_Request) :-
 paxos_quorum(_Request) :-
     paxos_property(quorum(A)),
     reply_json(json([quorum=A])).
+
+paxos_ledger(_Request) :-
+    findall(Key-Value, current_ledger(Key, Value), Data),
+    reply_json(json(Data), [serialize_unknown(true)]).
